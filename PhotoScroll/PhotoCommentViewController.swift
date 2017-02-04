8d9e871d6280dc44684f221f795cdad84a897b1d
RGP_KOREA <rgp_korea@RGP-KOREAui-MacBook-Pro.local>
2017-02-04 16:23:34 +0900

    first commit

---
+++ PhotoScroll/PhotoCommentViewController.swift
@@ -0,0 +1,75 @@
+//
+//  PhotoCommentViewController.swift
+//  PhotoScroll
+//
+//  Created by RGP_KOREA on 2017. 2. 4..
+//  Copyright © 2017년 raywenderlich. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCommentViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    var photoName: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let photoName = photoName {
+            imageView.image = UIImage(named: photoName)
+        }
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow(_:)),
+            name: NSNotification.Name.UIKeyboardWillShow,
+            object: nil)
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide(_:)),
+            name: NSNotification.Name.UIKeyboardWillHide,
+            object: nil)
+        
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @IBAction func hideKeyboard(_ sender: AnyObject) {
+        nameTextField.endEditing(true)
+    }
+    
+    func adjustInsetForKeyboardShow(show: Bool, notification: NSNotification) {
+        guard let value = notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue else { return
+        }
+        let keyboardFrame = value.cgRectValue
+        let adjustmentHeight = (keyboardFrame.height + 20) * (show ? 1 : -1)
+        scrollView.contentInset.bottom += adjustmentHeight
+        scrollView.scrollIndicatorInsets.bottom += adjustmentHeight
+    }
+    
+    func keyboardWillShow(_ notification: NSNotification) {
+        adjustInsetForKeyboardShow(show: true, notification: notification)
+    }
+    
+    func keyboardWillHide(_ notification: NSNotification) {
+        adjustInsetForKeyboardShow(show: false, notification: notification)
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
